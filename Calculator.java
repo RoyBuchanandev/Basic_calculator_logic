a6be3705756f87696b1543b56d0c009b190bf2a9
RoyBuchanandev <93665139+RoyBuchanandev@users.noreply.github.com>
2023-02-22 13:22:47 -0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -25,17 +25,17 @@ public class Calculator {
         return a / b;
     }
 
-     // metodo modulo -> retorna reste de a % b
-     public int modulo(int a, int b) {
+    // metodo modulo -> retorna reste de a % b
+    public int modulo(int a, int b) {
         return a % b;
     }
 
     public static void main(String[] args) {
-Calculator myCalculator = new Calculator();
-System.out.println(myCalculator.add(5, 7));
-System.out.println(myCalculator.substract(45, 11));
-System.out.println(myCalculator.multiply(2, 3));
-System.out.println(myCalculator.divide(2, 3));
-System.out.println(myCalculator.modulo(2, 3));
+        Calculator myCalculator = new Calculator();
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.substract(45, 11));
+        System.out.println(myCalculator.multiply(2, 3));
+        System.out.println(myCalculator.divide(2, 3));
+        System.out.println(myCalculator.modulo(2, 3));
     }
 }
